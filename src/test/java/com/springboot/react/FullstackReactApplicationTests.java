7cb16c68e6780177741eca931c779a607b45c835
Tanishka-dev <tanishakayadav2110@gmail.com>
2023-04-19 21:56:10 +0530

    initital commit

---
+++ src/test/java/com/springboot/react/FullstackReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
